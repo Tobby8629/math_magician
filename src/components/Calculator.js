b37f914451daadad59aa1ce08364a94529951b89
tobby8629 <sarhmullah15@gmail.com>
2023-02-13 11:45:00 -0800

    create event on each button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from './logic/calculate';
 
 const Calculator = () => {
   const digits = ['AC', '+/-', '%', '\u00f7', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-  const random = Math.ceil(Math.random * 1000);
+  const [solution, setsolution] = useState({ total: null, next: null, operation: null });
   return (
     <div className="calculator">
       <div className="digits">
-        <div className="input" />
+
+        <div className="input">
+          {solution.total}
+          {solution.operation}
+          {solution.next}
+        </div>
+
         <div className="grid">
           {digits.map((digit) => (
-            <div className="digit" key={random}>
+            <div
+              className="digit"
+              key={digit}
+              onClick={(e) => setsolution(calculate(solution, e.target.textContent))}
+              role="button"
+              tabIndex={0}
+              onKeyDown={(e) => setsolution(calculate(solution, e.target.textContent))}
+            >
               {digit}
             </div>
           ))}
         </div>
+
       </div>
     </div>
   );
